8b6404729310bcb25dc275c460f7dc80fd381ac4
ManukMelkonyan <myan.manuk@gmail.com>
2022-03-04 18:29:56 +0400

    fix(uploadPicture): picture upload fixed and working

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -106,77 +106,58 @@ module.exports.delete = (req, res) => {
 };
 
 module.exports.uploadPicture = (req, res) => {
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // endpoint: /users/:id/picture
+  // send image via multipart/form-data in the following format:
+  //                                                            (file key) -> profilePicture
+  //                                                            value -> file
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   const contentType = req.headers["content-type"];
-  // console.log({ contentType });
   const boundary = "--" + contentType.split(";")[1].split("=")[1];
-  // console.log({ boundary });
   let buf = new Buffer.alloc(0);
   req.on("data", (chunk) => {
-    // buf += chunk.toString();
     buf = Buffer.concat([buf, chunk]);
   });
   req.on("close", () => {
-    // console.log(buf.toString());
-    console.log("buf.length:  ", buf.length);
-    console.log("str length:  ", buf.toString("ascii").length);
-    console.log("new buf:  ", Buffer.from(buf.toString("ascii"), "ascii").length);
-    // fs.readFile(path.join(__dirname, "..", "..", fileName), (err, data) => {
-    //   data = data.toString();
-    //   Console.log("===========================", data.includes(buf.toString()));
-    //   Console.log("===========================", buf.toString().includes(data));
-    //   console.log("comparison finished");
-    // });
     const tokens = buf
       .toString("ascii")
       .split(boundary)
-      .map((elem) => elem.split("\r\n").filter((elem) => elem));
-    // console.log(tokens);
+      .map((elem) => elem.split("\r\n"))
+      .filter((elem) => elem);
+    console.log(
+      tokens.reduce((acc, curr) => {
+        if (!acc) acc++;
+        return acc;
+      }, 0)
+    );
     for (const token of tokens) {
       if (token.length === 0) continue;
+      if (!token[1]) continue;
 
-      const [disposition, name, fileInfo] = token[0].split(";");
+      console.log(
+        tokens.reduce((acc, curr) => {
+          if (!acc) acc++;
+          return acc;
+        }, 0)
+      );
+      const [disposition, name, fileInfo] = token[1].split(";");
       if (name && name.split("=")[1] === '"profilePicture"' && fileInfo.trim().startsWith("filename")) {
-        const content = token.slice(2).join("\r\n");
-        // const content = buf.toString().split(token[1])[1];
-        // console.log(token[2]);
-        console.log("token length ----------------------------------", token.length);
         const fileName = fileInfo.split("=")[1].replaceAll('"', "");
-        // console.log(fileName);
-        fs.writeFile(path.join(__dirname, "..", "receivedFiles", fileName), content, (err) => {
-          if (err) {
-            console.log(err);
-            res.status(500).send({
-              message: "Error while saving file",
-              error: err.message,
-            });
-          } else {
-            res.send({ message: "File saved successfully" });
+        fs.writeFile(
+          path.join(__dirname, "..", "receivedFiles", fileName),
+          buf.slice(buf.toString("ascii").indexOf(token[4][0]), buf.length - boundary.length - 6),
+          (err) => {
+            if (err) {
+              console.log(err);
+              res.status(500).send({
+                message: "Error while saving file",
+                error: err.message,
+              });
+            } else {
+              res.send({ message: "File saved successfully" });
+            }
           }
-          fs.readFile(path.join(__dirname, "..", "..", fileName), (err, data) => {
-            console.log("===========================", buf.includes(data));
-
-            // console.log(data.toString());
-            // console.log(data.toString().startsWith(content));
-            fs.readFile(path.join(__dirname, "..", "receivedFiles", fileName), (err, d) => {
-              if (err) {
-                console.log(err);
-              } else {
-                // data = data.toString();
-                // console.log("===========================", data.includes(buf.toString()));
-                // console.log("===========================", buf.toString().includes(data));
-                // // console.log("===========================", content.includes(data));
-                // console.log("===========================", data === d.toString());
-                // for (let i = 0; i < data.length; i++) {
-                //   if (data[i] !== content[i]) {
-                //     console.log("Not equal at: " + i);
-                //     break;
-                //   }
-                // }
-                // console.log("comparison finished");
-              }
-            });
-          });
-        });
+        );
         return;
       }
     }
--- test.js
+++ test.js
@@ -1,17 +1,28 @@
 const fs = require("fs");
 const path = require("path");
 
-fs.readFile(path.join(__dirname, "horse.jpg"), (err, data) => {
+const fileName = "dog.jpg";
+
+fs.readFile(path.join(__dirname, fileName), (err, data) => {
   if (err) {
     console.log(err);
   } else {
-    fs.readFile(path.join(__dirname, "app", "receivedFiles", "horse.jpg"), (err, d) => {
+    fs.readFile(path.join(__dirname, "app", "receivedFiles", fileName), (err, d) => {
       if (err) {
         console.log(err);
       } else {
+        console.log(data);
         console.log(d);
-        console.log(data.includes(d));
-        console.log(d.includes(data));
+        console.log(data.length);
+        console.log(d.length);
+        let counter = 0;
+        for (let i = 0; i < d.length; ++i) {
+          if (d[i] !== data[i]) {
+            counter++;
+          }
+        }
+
+        console.log("dismatch: " + counter);
 
         // console.log(data.toString() === d.toString());
       }
